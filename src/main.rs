bee2eaa8e1132cd6c192ef25a315d82b28773df0
Denis Papathanasiou <denis@papathanasiou.org>
2025-09-21 09:53:42 -0400

    Added env_logger crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use log::{error, info};
 use nix::errno::Errno;
 use nix::fcntl::{OFlag, open};
 use nix::sys::stat::Mode;
@@ -38,31 +39,33 @@ fn write_key_val(key: &str, val: &str) -> Result<usize, Errno> {
 }
 
 fn main() {
+    env_logger::init();
+
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         let prog = &args[0];
-        println!("Usage:\n\n{prog} (get|set) [key] [value (only with 'set')]");
+        error!("Usage:\n\n{prog} (get|set) [key] [value (only with 'set')]");
         std::process::exit(0);
     }
 
     let action = &args[1]; // "get" or "set"
     match action.as_str() {
         "get" => match read_key(&args[2]) {
-            Ok(data) => println!("success: {data}"),
+            Ok(data) => info!("success: {data}"),
             Err(e) => {
-                println!("syscall error {e}");
+                error!("syscall error {e}");
                 std::process::exit(-1);
             }
         },
         "set" => match write_key_val(&args[2], &args[3]) {
-            Ok(bytes) => println!("success: wrote {bytes} bytes"),
+            Ok(bytes) => info!("success: wrote {bytes} bytes"),
             Err(e) => {
-                println!("syscall error {e}");
+                error!("syscall error {e}");
                 std::process::exit(-1);
             }
         },
         _ => {
-            println!("error: invalid operation!");
+            error!("error: invalid operation!");
             std::process::exit(-1);
         }
     }
