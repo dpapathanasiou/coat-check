d5958e82989861a4ab418da01ae4ae6b55e0e327
Denis Papathanasiou <denis@papathanasiou.org>
2025-09-21 19:42:54 -0400

    Updated reader to iterate through the '[key][size of value][value]' byte arrays in the file, returning the first match or None if not found

--- src/file_syscalls.rs
+++ src/file_syscalls.rs
@@ -2,20 +2,50 @@ use crate::hasher;
 use nix::errno::Errno;
 use nix::fcntl::{OFlag, open};
 use nix::sys::stat::Mode;
-use nix::unistd::{close, read, write};
+use nix::unistd::{Whence, close, lseek, read, write};
 use std::os::unix::io::OwnedFd;
 
-pub fn read_key(filepath: String, key: &str, buffer: &mut [u8]) -> Result<usize, Errno> {
+pub fn read_key(filepath: String, key: &str) -> Result<Option<Vec<u8>>, Errno> {
     let fd: OwnedFd = open(filepath.as_str(), OFlag::O_RDONLY, Mode::empty())?;
-    let nbytes = read(&fd, buffer)?;
+
+    const SPACER: usize = std::mem::size_of::<usize>();
+    let size_buf: &mut [u8] = &mut vec![0; SPACER];
+    let mut sizer: [u8; SPACER] = [0; SPACER];
+
+    let hash = hasher::hash_key(key);
+    let key_buf: &mut [u8] = &mut vec![0; hash.len()];
+
+    // iterate through the `[(hashed) key][size of value][value]` byte arrays in file
+    let mut nbytes = read(&fd, key_buf)?;
+    while nbytes > 0 {
+        if hash != str::from_utf8(key_buf).unwrap() {
+            // no match at the current key position,
+            // so read the value size, and skip ahead
+            // to the next key/value array
+            _ = read(&fd, size_buf)?;
+            sizer.clone_from_slice(size_buf);
+            lseek(&fd, i64::from_ne_bytes(sizer), Whence::SeekCur)?;
+            nbytes = read(&fd, key_buf)?;
+        } else {
+            // matched, so get the value size, to read and return the value bytes
+            _ = read(&fd, size_buf)?;
+            sizer.clone_from_slice(size_buf);
+            let val_buf: &mut [u8] = &mut vec![0; usize::from_ne_bytes(sizer)];
+            _ = read(&fd, val_buf)?;
+            let mut result = Vec::new();
+            result.extend_from_slice(val_buf);
+            return Ok(Some(result));
+        }
+    }
+
     close(fd)?;
-    Ok(nbytes)
+    Ok(None)
 }
 
 pub fn write_key_val(filepath: String, key: &str, val: &[u8]) -> Result<usize, Errno> {
     let fd: OwnedFd = open(
         filepath.as_str(),
-        OFlag::O_WRONLY | OFlag::O_CREAT,
+        OFlag::O_WRONLY | OFlag::O_CREAT | OFlag::O_APPEND,
         Mode::S_IRUSR
             | Mode::S_IWUSR
             | Mode::S_IRGRP
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use coat_check::file_syscalls::{read_key, write_key_val};
 use log::{error, info};
 use std::env;
-use std::str::from_utf8;
 
 fn main() {
     env_logger::init();
@@ -17,23 +16,16 @@ fn main() {
 
     let action = &args[1]; // "get" or "set"
     match action.as_str() {
-        "get" => {
-            // TODO: size the read buffer according to the value size like this, once can get it from the serialize struct (upcoming logic change)
-            //let buf_size = &args[2].chars().count();
-            //let buffer: &mut [u8] = &mut vec![0; *buf_size];
-            // in the meantime, an arbitrary 256 size is ok
-            let buffer: &mut [u8] = &mut [0; 256];
-            match read_key(file_folder, &args[2], buffer) {
-                Ok(bytes) => info!(
-                    "success: wrote {bytes} bytes: {:?}",
-                    from_utf8(buffer).unwrap()
-                ),
-                Err(e) => {
-                    error!("syscall error {e}");
-                    std::process::exit(-1);
-                }
+        "get" => match read_key(file_folder, &args[2]) {
+            Ok(bytes) => match bytes {
+                Some(result) => info!("success: matched -> {:?}", String::from_utf8(result)),
+                None => info!("no match found"),
+            },
+            Err(e) => {
+                error!("syscall error {e}");
+                std::process::exit(-1);
             }
-        }
+        },
         "set" => match write_key_val(file_folder, &args[2], &args[3].as_bytes()) {
             Ok(bytes) => info!("success: wrote {bytes} bytes"),
             Err(e) => {
