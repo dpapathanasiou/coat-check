35b6749408cad5cc2b768cab0d89795af53ae8b5
Denis Papathanasiou <denis@papathanasiou.org>
2025-09-21 16:44:01 -0400

    Enforcing all reads and writes from one file, defined in env

--- src/file_syscalls.rs
+++ src/file_syscalls.rs
@@ -5,20 +5,14 @@ use nix::sys::stat::Mode;
 use nix::unistd::{close, read, write};
 use std::os::unix::io::OwnedFd;
 
-fn derive_filepath(folder: String, name: &str) -> String {
-    String::from(format!("/{}/{}.coat-check", folder, name))
-}
-
-pub fn read_key(folder: String, key: &str, buffer: &mut [u8]) -> Result<usize, Errno> {
-    let filepath = derive_filepath(folder, key);
+pub fn read_key(filepath: String, key: &str, buffer: &mut [u8]) -> Result<usize, Errno> {
     let fd: OwnedFd = open(filepath.as_str(), OFlag::O_RDONLY, Mode::empty())?;
     let nbytes = read(&fd, buffer)?;
     close(fd)?;
     Ok(nbytes)
 }
 
-pub fn write_key_val(folder: String, key: &str, val: &[u8]) -> Result<usize, Errno> {
-    let filepath = derive_filepath(folder, key);
+pub fn write_key_val(filepath: String, key: &str, val: &[u8]) -> Result<usize, Errno> {
     let fd: OwnedFd = open(
         filepath.as_str(),
         OFlag::O_WRONLY | OFlag::O_CREAT,
