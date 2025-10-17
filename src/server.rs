1bcbcff3ba630f3c8a1b888d0419cd1d3d0aeda5
Denis Papathanasiou <denis@papathanasiou.org>
2025-10-17 18:30:47 -0400

    Rewrote the client handler as a C function invoke by pthread_create, for every client connection

--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,110 @@
 use crate::file_syscalls::{read_key, write_key_val};
+use libc::{c_void, pthread_create, pthread_detach, pthread_t};
 use nix::errno::Errno;
 use nix::sys::socket::{
     AddressFamily, Backlog, MsgFlags, SockFlag, SockProtocol, SockType, SockaddrIn, accept, bind,
     listen, recv, send, socket,
 };
 use std::os::fd::{AsRawFd, RawFd};
+use std::{mem, ptr};
+
+#[repr(C)]
+struct ClientThreadArgs {
+    clientfd: RawFd,
+    filepath: String,
+}
+
+extern "C" fn handle_client(arg: *mut c_void) -> *mut c_void {
+    println!("Connected to client!");
+    let args = unsafe { Box::from_raw(arg as *mut ClientThreadArgs) };
+
+    let mut buf = [0u8; 1024];
+    let read_err_msg = String::from("Failed to read from client");
+    let write_err_msg = String::from("Failed to send to client");
+    let usage = String::from("Usage:\r\n<get> <key> | <set> <key> <value>");
+
+    let mut nbytes = recv(args.clientfd, &mut buf, MsgFlags::empty()).expect(&read_err_msg);
+    while nbytes > 0 {
+        let input_size = buf
+            .clone()
+            .iter()
+            .take_while(|c| **c != b'\n' && **c != b'\r')
+            .count();
+        if input_size > 0 {
+            // Split the byte array on spaces
+            let raw_input = buf.clone();
+            let parts: Vec<&[u8]> = raw_input[..input_size].split(|&b| b == b' ').collect();
+
+            // Reset the buffer for writing back to the client
+            buf.fill(0);
+            let mut replied: bool = false;
+
+            let cmd_size = parts.len();
+            if cmd_size > 1 {
+                let cmd = std::str::from_utf8(parts[0]).unwrap();
+                if cmd == "get" && cmd_size == 2 {
+                    match read_key(args.filepath.clone(), str::from_utf8(parts[1]).unwrap()) {
+                        Ok(bytes) => match bytes {
+                            Some(result) => {
+                                let r = result.len();
+                                buf[0..r].copy_from_slice(result.as_slice());
+                                buf[r..r + 2].copy_from_slice(b"\r\n")
+                            }
+                            None => {
+                                let no_match = String::from("*** no match found");
+                                let r = no_match.len();
+                                buf[0..r].copy_from_slice(no_match.as_bytes());
+                                buf[r..r + 2].copy_from_slice(b"\r\n")
+                            }
+                        },
+                        Err(e) => {
+                            let result = format!("*** error: {:?}", e.desc());
+                            let r = result.len();
+                            buf[0..r].copy_from_slice(result.as_bytes());
+                            buf[r..r + 2].copy_from_slice(b"\r\n")
+                        }
+                    };
+                    send(args.clientfd, &mut buf, MsgFlags::empty()).expect(&write_err_msg);
+                    replied = true;
+                } else if cmd == "set" && cmd_size > 2 {
+                    let key = str::from_utf8(parts[1]).unwrap();
+                    // val as the remaining input, after the key
+                    let val_start = key.len() + 5; // 5 = "set" and two spaces
+                    match write_key_val(
+                        args.filepath.clone(),
+                        key,
+                        &raw_input[val_start..input_size],
+                    ) {
+                        Ok(bytes) => {
+                            let result = format!("*** success: wrote {bytes} bytes");
+                            let r = result.len();
+                            buf[0..r].copy_from_slice(result.as_bytes());
+                            buf[r..r + 2].copy_from_slice(b"\r\n")
+                        }
+                        Err(e) => {
+                            let result = format!("*** error: {:?}", e.desc());
+                            let r = result.len();
+                            buf[0..r].copy_from_slice(result.as_bytes());
+                            buf[r..r + 2].copy_from_slice(b"\r\n")
+                        }
+                    }
+                    send(args.clientfd, &mut buf, MsgFlags::empty()).expect(&write_err_msg);
+                    replied = true;
+                }
+            };
+            if !replied {
+                let result = format!("*** invalid command\r\n{usage}");
+                let r = result.len();
+                buf[0..r].copy_from_slice(result.as_bytes());
+                buf[r..r + 2].copy_from_slice(b"\r\n");
+                send(args.clientfd, &mut buf, MsgFlags::empty()).expect(&write_err_msg);
+            }
+        }
+        nbytes = recv(args.clientfd, &mut buf, MsgFlags::empty()).expect(&read_err_msg);
+    }
+
+    ptr::null_mut()
+}
 
 #[derive(Debug)]
 pub struct Server {
@@ -38,91 +138,38 @@ impl Server {
     }
 
     pub fn handle(&self, sockfd: RawFd) {
-        let client = accept(sockfd).expect("Failed to accept client");
+        let mut connection = accept(sockfd);
+        while connection.is_ok() {
+            // Create a new pthread for each successful client connection
+            let args = ClientThreadArgs {
+                clientfd: connection.unwrap(),
+                filepath: self.filepath.clone(),
+            };
 
-        let mut buf = [0u8; 1024];
-        let read_err_msg = String::from("Failed to read from client");
-        let write_err_msg = String::from("Failed to send to client");
-        let usage = String::from("Usage:\r\n<get> <key> | <set> <key> <value>");
+            // Box the arguments to the client thread, so they do not go out of scope
+            let arg_ptr = Box::into_raw(Box::new(args));
 
-        let mut nbytes = recv(client, &mut buf, MsgFlags::empty()).expect(&read_err_msg);
-        while nbytes > 0 {
-            let input_size = buf
-                .clone()
-                .iter()
-                .take_while(|c| **c != b'\n' && **c != b'\r')
-                .count();
-            if input_size > 0 {
-                // Split the byte array on spaces
-                let raw_input = buf.clone();
-                let parts: Vec<&[u8]> = raw_input[..input_size].split(|&b| b == b' ').collect();
+            let mut thread_id: pthread_t = unsafe { mem::zeroed() };
+            let create_result = unsafe {
+                pthread_create(
+                    &mut thread_id,
+                    ptr::null(),
+                    handle_client,
+                    arg_ptr as *mut c_void,
+                )
+            };
 
-                // Reset the buffer for writing back to the client
-                buf.fill(0);
-                let mut replied: bool = false;
-
-                let cmd_size = parts.len();
-                if cmd_size > 1 {
-                    let cmd = std::str::from_utf8(parts[0]).unwrap();
-                    if cmd == "get" && cmd_size == 2 {
-                        match read_key(self.filepath.clone(), str::from_utf8(parts[1]).unwrap()) {
-                            Ok(bytes) => match bytes {
-                                Some(result) => {
-                                    let r = result.len();
-                                    buf[0..r].copy_from_slice(result.as_slice());
-                                    buf[r..r + 2].copy_from_slice(b"\r\n")
-                                }
-                                None => {
-                                    let no_match = String::from("*** no match found");
-                                    let r = no_match.len();
-                                    buf[0..r].copy_from_slice(no_match.as_bytes());
-                                    buf[r..r + 2].copy_from_slice(b"\r\n")
-                                }
-                            },
-                            Err(e) => {
-                                let result = format!("*** error: {:?}", e.desc());
-                                let r = result.len();
-                                buf[0..r].copy_from_slice(result.as_bytes());
-                                buf[r..r + 2].copy_from_slice(b"\r\n")
-                            }
-                        };
-                        send(client, &mut buf, MsgFlags::empty()).expect(&write_err_msg);
-                        replied = true;
-                    } else if cmd == "set" && cmd_size > 2 {
-                        let key = str::from_utf8(parts[1]).unwrap();
-                        // val as the remaining input, after the key
-                        let val_start = key.len() + 5; // 5 = "set" and two spaces
-                        match write_key_val(
-                            self.filepath.clone(),
-                            key,
-                            &raw_input[val_start..input_size],
-                        ) {
-                            Ok(bytes) => {
-                                let result = format!("*** success: wrote {bytes} bytes");
-                                let r = result.len();
-                                buf[0..r].copy_from_slice(result.as_bytes());
-                                buf[r..r + 2].copy_from_slice(b"\r\n")
-                            }
-                            Err(e) => {
-                                let result = format!("*** error: {:?}", e.desc());
-                                let r = result.len();
-                                buf[0..r].copy_from_slice(result.as_bytes());
-                                buf[r..r + 2].copy_from_slice(b"\r\n")
-                            }
-                        }
-                        send(client, &mut buf, MsgFlags::empty()).expect(&write_err_msg);
-                        replied = true;
-                    }
-                };
-                if !replied {
-                    let result = format!("*** invalid command\r\n{usage}");
-                    let r = result.len();
-                    buf[0..r].copy_from_slice(result.as_bytes());
-                    buf[r..r + 2].copy_from_slice(b"\r\n");
-                    send(client, &mut buf, MsgFlags::empty()).expect(&write_err_msg);
-                }
+            if create_result != 0 {
+                eprintln!("Error creating thread: {}", create_result);
             }
-            nbytes = recv(client, &mut buf, MsgFlags::empty()).expect(&read_err_msg);
+
+            // Let the newly-created thread run to completion
+            unsafe {
+                pthread_detach(thread_id);
+            }
+
+            // Accept any new client connections
+            connection = accept(sockfd);
         }
     }
 }
